02d47410d014898dec7df96214307706a835c2fa
tencentcloudapi <tencentcloudapi@tencent.com>
2025-12-18 11:19:54 +0800

    release 3.0.1505

---
+++ src/TencentCloud/Antiddos/V20200309/Models/DescribeBgpInstancesResponse.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Copyright (c) 2017-2025 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace TencentCloud\Antiddos\V20200309\Models;
+use TencentCloud\Common\AbstractModel;
+
+/**
+ * DescribeBgpInstances返回参数结构体
+ *
+ * @method integer getTotal() 获取返回数量
+ * @method void setTotal(integer $Total) 设置返回数量
+ * @method array getBGPInstanceList() 获取返回购买高防包信息
+ * @method void setBGPInstanceList(array $BGPInstanceList) 设置返回购买高防包信息
+ * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+ * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+ */
+class DescribeBgpInstancesResponse extends AbstractModel
+{
+    /**
+     * @var integer 返回数量
+     */
+    public $Total;
+
+    /**
+     * @var array 返回购买高防包信息
+     */
+    public $BGPInstanceList;
+
+    /**
+     * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+     */
+    public $RequestId;
+
+    /**
+     * @param integer $Total 返回数量
+     * @param array $BGPInstanceList 返回购买高防包信息
+     * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * For internal only. DO NOT USE IT.
+     */
+    public function deserialize($param)
+    {
+        if ($param === null) {
+            return;
+        }
+        if (array_key_exists("Total",$param) and $param["Total"] !== null) {
+            $this->Total = $param["Total"];
+        }
+
+        if (array_key_exists("BGPInstanceList",$param) and $param["BGPInstanceList"] !== null) {
+            $this->BGPInstanceList = [];
+            foreach ($param["BGPInstanceList"] as $key => $value){
+                $obj = new BGPInstanceInfo();
+                $obj->deserialize($value);
+                array_push($this->BGPInstanceList, $obj);
+            }
+        }
+
+        if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
+            $this->RequestId = $param["RequestId"];
+        }
+    }
+}
